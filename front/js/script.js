2296d4fd82be8a3411e8502788f300b22e5be627
Franck Olivier <allafranck@hotmail.com>
2022-11-03 14:01:11 +0100

    Ajout produit  page index

--- front/js/script.js
+++ front/js/script.js
@@ -6,7 +6,17 @@ fetch('http://localhost:3000/api/products')
             response.json()
             .then(function(products){
                 console.log(products)
-                //Continuer ici l'injection    
+                //Continuer ici l'injection   
+                for (let product of products){
+                    console.log(product)
+                    items.innerHTML += `<a href="./product.html?id=${product._id}">
+                    <article>
+                      <img src="${product.imageUrl}" alt="${product.altTxt}">
+                      <h3 class="productName">${product.name}</h3>
+                      <p class="productDescription">${product.description}</p>
+                    </article>
+                  </a>`
+                } 
             })
             .catch(function(error){
                 console.log(error)
